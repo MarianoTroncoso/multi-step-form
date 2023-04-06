22f5b4b12af3f6151fc51ead5b8aa47a70675ccc
Mariano Troncoso <mtroncoso@litebox.ai>
2023-04-06 18:28:53 -0300

    Add props in FormStep

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@ import FormStep from './components/FomStep/FomStep';
 import StepList from './components/StepList';
 import * as SC from './App.styles';
 
+const TEMP_FORM_STEP_INFO = {
+  title: 'Step 1',
+  description: 'This is the first step',
+};
+
 function App() {
   return (
     <>
       <GlobalStyle />
       <SC.Wrapper>
         <StepList />
-        <FormStep />
+        <FormStep
+          title={TEMP_FORM_STEP_INFO.title}
+          description={TEMP_FORM_STEP_INFO.description}
+        >
+          some form
+        </FormStep>
       </SC.Wrapper>
     </>
   );
--- src/components/FomStep/FomStep.tsx
+++ src/components/FomStep/FomStep.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import * as SC from './FormStep.styles';
 
-type Props = {};
+type Props = {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+  showGoBackButton?: boolean;
+  rightButtonText?: string;
+};
 
-const FormStep: React.FC<Props> = () => {
-  return <SC.Wrapper>FormStep</SC.Wrapper>;
+const FormStep: React.FC<Props> = ({
+  title,
+  description,
+  children,
+  showGoBackButton = true,
+  rightButtonText = 'Next Step',
+}) => {
+  return (
+    <SC.Wrapper>
+      <div>{title}</div>
+      <div>{description}</div>
+      {children}
+    </SC.Wrapper>
+  );
 };
 
 export default FormStep;
