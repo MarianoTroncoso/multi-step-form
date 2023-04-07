90d66c164dc9cc3ea97df4ff6f079010f29322c7
Mariano Troncoso <mtroncoso@litebox.ai>
2023-04-07 12:15:11 -0300

    Add basics step list styles

--- src/components/StepList/StepList.styles.ts
+++ src/components/StepList/StepList.styles.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  border: 1px solid blue;
   width: 40%;
+  background-color: ${({ theme }) => theme.colors.primary[1]};
+  border-radius: 0.5rem;
+  padding: 2rem;
+  color: ${({ theme }) => theme.colors.neutral[4]};
 `;
